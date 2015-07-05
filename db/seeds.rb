0204f7e1284682c871b2187e6bb32f63cdf56a6d
Khanh Nguyen <baokhanh_n@hotmail.com>
2015-07-05 21:24:22 +0700

    Update db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ note_list = ['Very gooddddddddddddddddddddddddddd', 'lul this suck', "that bitch
 
 image = ["http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/9/24/1411574454561/03085543-87de-47ab-a4eb-58e7e39d022e-620x372.jpeg", ""]
 
-vn_price = [500000, 1000000, 10050040, 230000, 45500, 3530000, 100000, 36000000, 52000000, 3421200, 3000]
+vn_price = [500000, 1000000, 10050040, 230000, 45500, 3530000, 100000, 36000000, 52000000, 3421200]
 
 price_list = [3.99, 345.99, 45, 10.50, 0, 10500, 1400, 860, 640.99, 150, 40.50, 79.99, 83.40]
 customer_array = []
@@ -33,14 +33,28 @@ customer_array = []
   customer_array << new_customer.id
 end
 
-20.times do
+30.times do
   s = rand(100)
   quan = rand(1..20)
   price = price_list[rand(0..12)]
   vn = vn_price[rand(0..10)]
+  tax = (price * 0.08).round(2)
+  reward = (price * 0.01).round(2)
+  deposit = (vn.to_f * 0.1).round(2)
+  total = price + tax + quan - reward
+  profit = vn.to_f - (total * 21000)
   Order.create(name: order_name[rand(0..6)], quantity: quan, description: note_list[rand(0..8)], note: note_list[rand(0..8)], 
                store: store_name[rand(0..9)], image_link: image[rand(0..1)], 
                order_date: Time.now, receive_us: 2.days.from_now, ship_vn: 3.days.from_now, 
-               web_price: price, tax: (price * 0.08).round(2), shipping_us: quan, reward: (price * 0.01).round(2), 
-               vnd: 21000, deposit: (vn * 0.1).round(2), selling_price: vn, user_id: 1, customer_id: customer_array[rand(customer_array.size) - 1])
+               web_price: price, tax: tax, shipping_us: quan, reward: (price * 0.01).round(2), total: total.round(2), total_cost: total.round(2),
+               profit: profit.round(2), vnd: 21000, deposit: deposit, selling_price: vn, user_id: 1, customer_id: customer_array[rand(customer_array.size) - 1])
 end
+
+
+
+
+
+
+
+
+
