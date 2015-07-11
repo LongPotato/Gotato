d40ead611d52fb008c36c77f341301b553fc633c
Khanh Nguyen <baokhanh_n@hotmail.com>
2015-07-11 16:45:29 +0700

    Fix hardcode issuse

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJYSJ4DQL5DQTJQWQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['longpotatopictures']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
