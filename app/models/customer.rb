060f0c1f8d313b2ad33cece1de97f3bc8eb7cfda
Khanh Nguyen <baokhanh_n@hotmail.com>
2015-07-12 21:10:29 +0700

    Update shipment show & customer search

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -27,7 +27,7 @@ class Customer < ActiveRecord::Base
 
   def self.search(search)
     if search
-      where('name LIKE ?', "%#{search}%")
+      where('name LIKE ?', "%#{search.downcase}%")
     else
       all
     end
