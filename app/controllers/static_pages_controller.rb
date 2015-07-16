78ec3da7928990e434cc0ac08a36b6b9b1bce9ed
Khanh Nguyen <baokhanh_n@hotmail.com>
2015-07-16 20:09:09 +0700

    Fix feed nil bug when log out

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
 
   def home
-    @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.id).limit(10).uniq
+    if current_user
+      @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.id).limit(10).uniq
+    end
   end
 
 end
