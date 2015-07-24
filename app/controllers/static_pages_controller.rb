5b4c91cefe8c039577b25374c314911d6b47f236
Khanh Nguyen <baokhanh_n@hotmail.com>
2015-07-24 18:33:49 +0700

    Add order progress bar to homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,15 @@ class StaticPagesController < ApplicationController
 
   def home
     if current_user
-      @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.id).limit(10).uniq
+      @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.id).limit(5).uniq
+      @this_month = current_user.data.where("month_record BETWEEN ? AND ?", Time.now.beginning_of_month, Time.now.end_of_month).first
+      completed = @this_month.orders.where.not("ship_vn" => nil).received.count
+      total = @this_month.order_sold
+      received = @this_month.orders.received.where("ship_vn" => nil).count
+
+      @completed = ((completed.to_f / total) * 100).floor
+      @received = ((received.to_f / total) * 100).floor
+      @not_received = 100 - @completed - @received
     end
   end
 
