8eaeb85259c8e9d0e999bd19af8bdb493fe824ba
Khanh Nguyen <baokhanh_n@hotmail.com>
2015-07-25 23:52:09 +0700

    Fix nil homepage problem for new user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,13 +4,15 @@ class StaticPagesController < ApplicationController
     if current_user
       @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.id).limit(5).uniq
       @this_month = current_user.data.where("month_record BETWEEN ? AND ?", Time.now.beginning_of_month, Time.now.end_of_month).first
-      completed = @this_month.orders.where.not("ship_vn" => nil).received.count
-      total = @this_month.order_sold
-      received = @this_month.orders.where("ship_vn" => nil).received.count
+      unless @this_month.nil?
+        completed = @this_month.orders.where.not("ship_vn" => nil).received.count
+        total = @this_month.order_sold
+        received = @this_month.orders.where("ship_vn" => nil).received.count
 
-      @completed = ((completed.to_f / total) * 100)
-      @received = ((received.to_f / total) * 100)
-      @not_received = 100 - @completed - @received
+        @completed = ((completed.to_f / total) * 100)
+        @received = ((received.to_f / total) * 100)
+        @not_received = 100 - @completed - @received
+      end
     end
   end
 
