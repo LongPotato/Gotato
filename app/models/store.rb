0a079cba14bc76ab5e591d89bf8ed0c52de46afd
Khanh Nguyen <baokhanh_n@hotmail.com>
2015-08-24 18:55:14 -0700

    Add activity tracker to store

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,7 @@
 class Store < ActiveRecord::Base
+  include PublicActivity::Model
+  tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
+
   belongs_to :user
   has_many :orders
 
