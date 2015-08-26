d0a4aa7cf5493c17cd55da06faa18ac353c2b828
Khanh Nguyen <baokhanh_n@hotmail.com>
2015-08-25 21:54:13 -0700

    Add greeting message to homepage

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,21 @@
 module StaticPagesHelper
+
+  def greeting_message
+    current_time = Time.now.to_i
+    midnight = Time.now.beginning_of_day.to_i
+    noon = Time.now.middle_of_day.to_i
+    five_pm = Time.now.change(:hour => 17 ).to_i
+    eight_pm = Time.now.change(:hour => 20 ).to_i
+
+    if midnight.upto(noon).include?(current_time)
+      "Good morning"
+    elsif noon.upto(five_pm).include?(current_time)
+      "Good afternoon"
+    elsif five_pm.upto(eight_pm).include?(current_time)
+      "Good evening"
+    elsif eight_pm.upto(midnight + 1.day).include?(current_time)
+      "Good night"
+    end
+  end
+
 end
