88c560923b27e9e5214001a2a1b7f61d9d0fdc48
Khanh Nguyen <baokhanh_n@hotmail.com>
2015-09-03 15:25:44 -0700

    test change

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -1,10 +1,14 @@
-function CallServerFunction() {
+$( document ).ready(function() {
+
+$( ".trigger-click" ).click(function() {
   $('#loadingModal').modal('show');
-  $.ajax({ url: "", success: function (data) 
+  $.ajax({ url: "http://localhost:3000/users/3/report", success: function (data) 
           { $('#loadingModal').modal('hide');
             console.log('The page has been successfully loaded');
           }, error: function () {
                     $('#loadingModal').modal('hide');
                     console.log('An error occurred'); }
          });
-}
+    });
+    
+});
