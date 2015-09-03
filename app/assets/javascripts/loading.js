b2421d5f5f7c97740141716b132e261c78498fc0
Khanh Nguyen <baokhanh_n@hotmail.com>
2015-09-03 15:28:50 -0700

    Revert back to original

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -1,14 +1,10 @@
-$( document ).ready(function() {
-
-$( ".trigger-click" ).click(function() {
+function CallServerFunction() {
   $('#loadingModal').modal('show');
-  $.ajax({ url: "http://localhost:3000/users/3/report", success: function (data) 
+  $.ajax({ url: "", success: function (data) 
           { $('#loadingModal').modal('hide');
             console.log('The page has been successfully loaded');
           }, error: function () {
                     $('#loadingModal').modal('hide');
                     console.log('An error occurred'); }
          });
-    });
-    
-});
+}
